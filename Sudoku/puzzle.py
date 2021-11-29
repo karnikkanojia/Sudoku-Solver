53ba0ca7dea1aceb6505f57c484d0f34545c430b
karnikkanojia <karnikk1406120@gmail.com>
2021-11-29 11:46:06 +0530

    OCR code and functions added. Testing left

--- Sudoku/puzzle.py
+++ Sudoku/puzzle.py
@@ -112,6 +112,55 @@ def find_puzzle(image, debug=True):
     return puzzle, warped
 
 def extract_digits(cell, debug=True):
-    # apply automatic thresholding to the cell and then clear any
-	# connected borders that touch the border of the cell
+    # apply automatic thresholding to the cell and then clear
+    # any connected border that touch the border of the cell
+    thresh = cv2.threshold(cell, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+    thresh = clear_border(thresh)
 
+    # check to see if we are visualizing the cell thresholding step
+    if debug:
+        cv2.imshow("Cell Thresh", thresh)
+        cv2.waitKey(0)
+
+    # find contours in the thresholded cells
+    cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = grab_contours(cnts)
+
+    # if no contours found that is a empty cell
+    if not len(cnts):
+        return None
+    
+    # otherwise, find the largest contour in the cell and create a
+	# mask for the contour
+    c = max(cnts, key=cv2.contourArea)
+    mask = np.zeros(thresh.shape, dtype='uint8')
+    cv2.drawContours(mask, [c], -1, 255, -1)
+    cnts = grab_contours(cnts)
+
+    # if no contours were found then this is an empty cell
+    if len(cnts) == 0:
+        return None
+
+    # otherwise find the largest contour in the cell and create a mask for the contour
+    c = max(cnts, key=cv2.contourArea)
+    mask = np.zeros(thresh.shape, dtype='uint8')
+    cv2.drawContours(mask, [c], -1, 255, -1)
+
+    # compute the percentage of masked pixels relative to the total area of image
+    (h, w) = thresh.shape
+    percentfilled = cv2.countNonZero(mask)/float(w*h)
+
+    # if less than 3% of the mask is filled then noise and can safely ignore the contour
+    if percentfilled < 0.03:
+        return None
+
+    # apply the mask to the thresholded cell
+    digit = cv2.bitwise_and(thresh, thresh, mask=mask)
+
+    # check to see if we should visualize the masking step
+    if debug:
+        cv2.imshow('Digit', digit)
+        cv2.waitKey(0)
+    
+    return digit
+    
