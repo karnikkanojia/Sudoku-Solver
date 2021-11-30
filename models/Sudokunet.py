8f63ab94432501237713acb83c9fd8aa06d751b9
karnikkanojia <karnikk1406120@gmail.com>
2021-11-30 08:55:55 +0530

    Model Architecture Changed. Achieved accuracy of 0.996.

--- models/Sudokunet.py
+++ models/Sudokunet.py
@@ -1,27 +1,32 @@
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dropout, Conv2D, MaxPool2D, BatchNormalization, Dense, Flatten
+from tensorflow.keras.layers import Dropout, Conv2D, MaxPool2D, BatchNormalization,Dense, Flatten
 
 class SudokuNet:
     @staticmethod
     def build( height, width, depth, classes):
         model = Sequential()
         input_shape = (height, width, depth)
-        model.add(Conv2D(32, (5, 5), padding="same", input_shape=input_shape, activation='relu'))
-		model.add(MaxPool2D(pool_size=(2, 2)))
-		# second set of CONV => RELU => POOL layers
-		model.add(Conv2D(32, (3, 3), padding="same", activation='relu'))
-		model.add(MaxPool2D(pool_size=(2, 2)))
-		# first set of FC => RELU layers
-		model.add(Flatten())
-		model.add(Dense(64), activation='relu')
-		model.add(Dropout(0.5))
-		# second set of FC => RELU layers
-		model.add(Dense(64), activation='relu')
-		model.add(Dropout(0.5))
-		# softmax classifier
-		model.add(Dense(classes), activation='softmax')
-		# return the constructed network architecture
-		return model
+        model.add(Conv2D(32,kernel_size=3,activation='relu',input_shape=input_shape))
+        model.add(BatchNormalization())
+        model.add(Conv2D(32,kernel_size=3,activation='relu'))
+        model.add(BatchNormalization())
+        model.add(Conv2D(32,kernel_size=5,strides=2,padding='same',activation='relu'))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.4))
+
+        model.add(Conv2D(64,kernel_size=3,activation='relu'))
+        model.add(BatchNormalization())
+        model.add(Conv2D(64,kernel_size=3,activation='relu'))
+        model.add(BatchNormalization())
+        model.add(Conv2D(64,kernel_size=5,strides=2,padding='same',activation='relu'))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.4))
+
+        model.add(Flatten())
+        model.add(Dense(128, activation='relu'))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.4))
+        model.add(Dense(classes, activation='softmax'))
 
         return model
